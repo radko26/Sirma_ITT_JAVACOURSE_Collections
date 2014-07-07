2365d9f4847de43a009ee7964a0e4871b2c2c3f5
radko26 <radipirate@gmail.com>
2014-07-07 11:42:42 +0300

    update

--- collections/module-collections/src/test/java/com/sirma/itt/javacourse/collections/exmsgmanager/ExceptionMessageManagerTest.java
+++ collections/module-collections/src/test/java/com/sirma/itt/javacourse/collections/exmsgmanager/ExceptionMessageManagerTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.Collection;
 import java.util.HashMap;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -13,27 +14,59 @@ import org.junit.Test;
  * @author radoslav
  */
 public class ExceptionMessageManagerTest {
+	private HashMap<String, String> table;
+	private ExceptionMessageManager manager;
+
+	/**
+	 * Initializes the global variables.
+	 */
+	@Before
+	public void init() {
+		table = new HashMap<String, String>();
+		table.put("1", "Wrong name");
+		table.put("2", "You are crossing the line, sir!");
+		manager = new ExceptionMessageManager(table);
+	}
 
 	/**
 	 * Adds an exceptionMessage to manager and compare the output with the
 	 * previous input.
 	 * 
 	 * @throws Exception
-	 *             In case of wrong input regarding to the given task of the
-	 *             problem.
+	 *             In case of trying to add a message which does not exist in
+	 *             the exception table.
 	 */
 	@Test
 	public void testAdd() throws Exception {
-		HashMap<String, String> table = new HashMap<String, String>();
-		table.put("1", "Wrong name");
+		int counter = 0;
 
-		ExceptionMessageManager manager = new ExceptionMessageManager(table);
 		manager.addExceptionMessage("Wrong name");
 		manager.addExceptionMessageUsingCode("1");
-		Collection<String> output = ExceptionMessageManager.getMessages(manager.getMessage());
+		manager.addExceptionMessageUsingCode("2");
+
+		Collection<String> output = ExceptionMessageManager.getMessages(manager
+				.getMessage());
+
 		for (String msg : output) {
-			assertEquals(msg, "Wrong name");
+			if (counter < 2) {
+				assertEquals(msg, "Wrong name");
+			} else {
+				assertEquals(msg, "You are crossing the line, sir!");
+			}
+			counter++;
 		}
+	}
+
+	/**
+	 * Tries to add a message which does not exist in the exception table.
+	 * 
+	 * @throws Exception
+	 *             In case of trying to add a message which does not exist in
+	 *             the exception table.
+	 */
+	@Test(expected = Exception.class)
+	public void testAddWithBadCode() throws Exception {
+		manager.addExceptionMessageUsingCode("4");
 	}
 
 }
